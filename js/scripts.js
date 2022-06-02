2ca24e819af92fce1ba72206d4fed2b8f2fdcabd
Lakshmi Sathyan <lakshmisathyan0704@gmail.com>
2022-06-02 16:05:17 +0530

    added js for first calculator

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,13 @@ window.addEventListener("load", function() {
 //     document.getElementById("loan1").style.display = "block";
 
 // }
+
+function calc1() {
+
+    var m = parseFloat(document.getElementById("monthlypayment").value);
+    var i = parseFloat(document.getElementById("interestrate1").value) / 1200;
+    var n = parseFloat(document.getElementById("n1").value);
+    var la = ((m * (1 - (1 / (Math.pow((1 + i), n))))) / i);
+    var loan = document.getElementById("la");
+    loan.innerHTML = la;
+}
