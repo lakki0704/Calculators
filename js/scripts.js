802df54f0845d11d642ad543b42c46883c43201b
Lakshmi Sathyan <lakshmisathyan0704@gmail.com>
2022-06-02 15:15:27 +0530

    customised calculator pages

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,14 @@ window.addEventListener("load", function() {
 })
 
 
-function openForm() {
-    document.getElementById("myform").style.display = "block";
-    document.getElementById("loan1").style.display = "none";
+// function openForm() {
+//     document.getElementById("myform").style.display = "block";
+//     document.getElementById("loan1").style.display = "none";
 
-}
+// }
 
-function closeForm() {
-    document.getElementById("myform").style.display = "none";
-    document.getElementById("loan1").style.display = "block";
+// function closeForm() {
+//     document.getElementById("myform").style.display = "none";
+//     document.getElementById("loan1").style.display = "block";
 
-}
+// }
