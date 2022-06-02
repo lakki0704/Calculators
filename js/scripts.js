f610713110aa293dd2b29cfe1979bcad5935f28a
Lakshmi Sathyan <lakshmisathyan0704@gmail.com>
2022-06-02 12:51:05 +0530

    shifted side bar to right

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,16 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load", function() {
     loader.style.display = "none";
 })
+
+
+function openForm() {
+    document.getElementById("myform").style.display = "block";
+    document.getElementById("loan1").style.display = "none";
+
+}
+
+function closeForm() {
+    document.getElementById("myform").style.display = "none";
+    document.getElementById("loan1").style.display = "block";
+
+}
