b51b652560b086ac8be8c88fa5784875a89cd4ef
Lakshmi Sathyan <lakshmisathyan0704@gmail.com>
2022-06-03 14:03:32 +0530

    made second calculator and added a 'goto top' button

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,24 @@ window.addEventListener("load", function() {
 
 // }
 
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
 function calc1() {
 
     var m = parseFloat(document.getElementById("monthlypayment").value);
@@ -24,4 +42,15 @@ function calc1() {
     var la = ((m * (1 - (1 / (Math.pow((1 + i), n))))) / i);
     var loan = document.getElementById("la");
     loan.innerHTML = la;
+}
+
+
+function calc2() {
+
+    var m = parseFloat(document.getElementById("loanamount2").value);
+    var i = parseFloat(document.getElementById("interestrate2").value) / 100;
+    var n = parseFloat(document.getElementById("n2").value);
+    var pay = (m * i * (Math.pow((1 + i), n))) / ((Math.pow((1 + i), n)) - 1);
+    var payment = document.getElementById("payment2");
+    payment.innerHTML = pay;
 }
