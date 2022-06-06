ef400d5d501363cdddf1cde232f7f495aedcd776
Lakshmi Sathyan <lakshmisathyan0704@gmail.com>
2022-06-06 23:02:46 +0530

    changed the front page

--- js/scripts.js
+++ js/scripts.js
@@ -194,3 +194,18 @@ function calc15() {
     var payment = document.getElementById("payment15");
     payment.innerHTML = pay.toFixed(3);
 }
+
+
+function search() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('calculatorz');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        } else {
+            x[i].style.display = "list-item";
+        }
+    }
+}
